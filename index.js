f52c9f5ed62487835d1a6e804e53a0c1c1de1575
Kole Dallam <kolejasonD@gmail.com>
2023-04-26 12:31:54 -0600

    inquirer prompt

--- index.js
+++ index.js
@@ -16,3 +16,36 @@ let team = {
     interns: []
 };
 
+console.log("Welcome to Sumtwelve's Team Page Generator!");
+console.log("\nA series of prompts will guide you through creating a webpage\nthat will store useful info and links about your team members.\n");
+console.log("To begin, you must enter a Team Name and at least one Team Manager.\n")
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is your team called?",
+            name: "teamName"
+        },
+        {
+            type: "input",
+            message: "Enter the following for your Team Manager\nName:",
+            name: "mgrName"
+        },
+        {
+            type: "input",
+            message: "Employee ID:",
+            name: "mgrID"
+        },
+        {
+            type: "input",
+            message: "Email address:",
+            name: "mgrEmail"
+        },
+        {
+            type: "input",
+            message: "Office number:",
+            name: "mgrOfficeNum"
+        }
+    ])
+    
