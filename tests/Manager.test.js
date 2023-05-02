9c8fdb3a92f90e5059ad2f72f2d1656f477aaa31
Kole Dallam <kolejasonD@gmail.com>
2023-05-01 21:23:15 -0600

    jjjj

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,4 +1,4 @@
-const Manager = require('./lib/manager');
+const Manager = require('../lib/manager');
 
 describe('Manager', () => {
     it('should hold solid references to its properties', () => {
