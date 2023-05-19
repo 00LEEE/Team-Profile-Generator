79231d3c13ad4d411ed1f36003ad669cc1234a2b
Kole Dallam <kolejasonD@gmail.com>
2023-05-19 14:12:00 -0600

    --

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const Employee = require("./lib/employee");
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Employee = require("../lib/employee");
+const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
 
 const generateHTML = require("./src/generateHTML");
 const generateCSS = require("./src/generateCSS");
